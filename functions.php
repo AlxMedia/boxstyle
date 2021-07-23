a0ec82c55a4a41f5c9ee60e2b185796ff51d3898
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-07-23 18:44:33 +0200

    Disable support for widgets block editor, regen lang

--- functions.php
+++ functions.php
@@ -66,6 +66,9 @@ if ( ! function_exists( 'boxstyle_setup' ) ) {
 		// Enable support for selective refresh of widgets in customizer
 		add_theme_support( 'customize-selective-refresh-widgets' );
 		
+		// Disable support for widgets block editor
+		remove_theme_support( 'widgets-block-editor' );
+		
 		// Thumbnail sizes
 		add_image_size( 'boxstyle-small', 200, 200, true );
 		add_image_size( 'boxstyle-medium', 520, 292, true );
